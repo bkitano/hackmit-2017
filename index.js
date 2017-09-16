28812ab39f4a27e1fdedc8cf3ce0a69a432e732c
Brian <brian.kitano@yale.edu>
2017-09-16 17:49:36 +0000

    connected to database

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 // CONSTANTS
-
+var assert = require("assert");
 var express = require('express');
 var exphbs  = require('express-handlebars');
-const app = express()
+var MongoClient = require("mongodb").MongoClient;
+
+var passport = require('passport');
+var config = require('./oauth.js');
+var FacebookStrategy = require('passport-facebook').Strategy;
+
 
 // MIDDLEWARE
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
 
+// express
+const app = express()
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
+// handlebars
+app.set('view engine', 'handlebars');
+app.use('/assets', express.static(__dirname + '/assets'));
+
+// mongo
+var mongourl = "mongodb://matteo:1234@ds139124.mlab.com:39124/hackmit-2017"
+
+// check to make sure the database is connected at the start
+MongoClient.connect(mongourl, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to database");
+  db.close();
+});
 
 // ROUTES
 app.get('/', function (req, res) {
